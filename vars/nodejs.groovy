cab02a4fa4de85b65399404fdedbbe6867fcfc46
dotunakin <dotunprom@gmail.com>
2022-10-17 23:32:08 +0100

    Parameter

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@ def lintChecks() {
   sh '''
     #we comment this because devs gonna check the failure.
     #~/node_modules/jslint/bin/jslint.js server.js
-    echo Link Check
+    echo Link Check for COMPONENT
   '''
 }
 
