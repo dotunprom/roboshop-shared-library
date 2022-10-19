e592659babf737358f19d14d049303246bed20e3
dotunakin <dotunprom@gmail.com>
2022-10-19 02:36:04 +0100

    Parameter

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,9 +6,21 @@ def lintChecks() {
   '''
 }
 
+
+def sonarCheck() {
+  sh'''
+   sonar-scanner -Dsonar.host.url=http://172.31.10.237:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
+  '''
+
+}
+
 def call() {
   pipeline {
     agent any
+
+    environment {
+      SONAR = credentials('SONAR')
+    }
 
     stages {
 
@@ -17,6 +29,15 @@ def call() {
         steps {
           script {
             lintChecks()
+          }
+        }
+      }
+
+
+      stage('Sonar Checks') {
+        steps {
+          script {
+            sonarChecks()
           }
         }
       }
